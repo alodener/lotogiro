30629a3b604ad123622242d88d016a00d02f1223
Cesar Damascena <cesar.damascena@digital2b.com.br>
2022-07-23 19:28:46 -0300

    make login page translatable

---
+++ resources/lang/pt_BR/admin.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'login-title' => 'Realize o login para iniciar a sessão',
+    'email-field' => 'E-mail',
+    'password-field' => 'Senha',
+    'keep-connected' => 'Manter conectado',
+    'login-button' => 'Acessar',
+    'forgot-password-link' => 'Esqueceu sua Senha?',
+    'register-label' => 'Não é cadastrado?',
+    'register-button' => 'Cadastre-se',
+    'consultant-link-text' => 'Deseja ser um consultor?'
+];
