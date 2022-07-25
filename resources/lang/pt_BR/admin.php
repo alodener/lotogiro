d14f27838565866a763d6e8d8e31f84864d801cd
Cesar Damascena <cesar.damascena@digital2b.com.br>
2022-07-25 19:55:55 -0300

    make dashboard translatable

--- resources/lang/pt_BR/admin.php
+++ resources/lang/pt_BR/admin.php
@@ -10,6 +10,7 @@ return [
     'register-label' => 'Não é cadastrado?',
     'register-button' => 'Cadastre-se',
     'consultant-link-text' => 'Deseja ser um consultor?',
+    'copy-link-button' => 'Copiar Link',
 
     'register' => [
         'page-title' => 'Cadastro',
@@ -23,5 +24,31 @@ return [
         'phone-label' => 'Telefone',
         'password-label' => 'Senha',
         'confirm-password-label'=> 'Confirmar Senha',
+    ],
+
+    'dashboard' => [
+        'page-title' => 'Inicio',
+        'page-header' => 'JOGOS',
+        'games-done-title' => 'Jogos Feitos',
+        'balance-title' => 'Saldo',
+        'copy-link-message' => 'Clique no botão e copie seu link acima',
+        'copy-whatsapp-button' => 'Enviar via WhatsApp',
+        'copy-whatspp-message' => 'Clique no botão e envie pelo WhatsApp',
+        'referral-button-text' => 'Indique e Ganhe!',
+        'referral-messsage' => 'Clique no botão e copie seu link de indicação',
+        'referrals-button' => 'Seus indicados',
+        'referrals-message' => 'Clique no botão e veja seus indicados',
+        'points-title' => 'Pontuações e Qualificações',
+        'personal-points' => 'Pontos Pessoais',
+        'group-points' => 'Pontos de Grupo',
+        'total-points' => 'Pontos Totais',
+        'your-qualification' => 'Sua Qualificação',
+        'personal-points-used' => 'Aproveitamento Pessoal',
+        'group-points-used' => 'Aproveitamento Grupo',
+        'points-next-qualification' => 'Quanto pontos faltam para o próxima qualificação',
+        'max-points-title' => 'Parabéns você esta no topo',
+        'max-points-text' => 'Você esta no mais alto nível do plano de carreira',
+        'games-not-found' => 'Não existem tipos de jogos cadastrados!',
+        'copied-url' => 'Link copiado!'
     ]
 ];
