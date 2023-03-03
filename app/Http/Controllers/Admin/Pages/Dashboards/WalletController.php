36148f643d7d79c71bb7383a0d44c733cbb20345
Dener Gomes <52760360+alodener@users.noreply.github.com>
2023-03-03 16:44:34 -0300

    update correção WalletController

--- app/Http/Controllers/Admin/Pages/Dashboards/WalletController.php
+++ app/Http/Controllers/Admin/Pages/Dashboards/WalletController.php
@@ -107,7 +107,7 @@
                             'user_id' => $user->id,
                             'value' => $totalRecharge,
                             'old_value' => $user->balance,
-                            'value_a' => auth()->user()->balance - $request->value,
+                            'value_a' => $user->balance + $totalRecharge,
                             'type' => "Recarga efetuada por meio da plataforma. {$msgCommission}"
                         ]);
 
