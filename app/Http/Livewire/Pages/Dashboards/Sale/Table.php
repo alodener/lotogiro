42a823e91415ae8717ce7ecce5e98d4b50a6bcb9
brunaroberta <123129885+brunaroberta@users.noreply.github.com>
2023-11-29 16:08:13 -0300

    Update Table.php

--- app/Http/Livewire/Pages/Dashboards/Sale/Table.php
+++ app/Http/Livewire/Pages/Dashboards/Sale/Table.php
@@ -255,7 +255,7 @@ class Table extends Component
         $filterRange = $this->filterRange();
         $query
             ->when($this->range, fn($query, $search) => $query->whereDate('created_at', '>=', $filterRange['dateStart'])
-                ->whereDate('created_at', '<=', $filterRange['dateEnd']));
+                ->whereDate('created_at', '<=', $filterRange['dateEnd'])
                 ->orderBy('created_at', 'desc'));
         
         $query = $this->filterUser($query);
