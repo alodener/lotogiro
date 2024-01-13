1691820e6ad812448555892060f9008c77417fda
Dener Gomes <52760360+alodener@users.noreply.github.com>
2024-01-13 01:56:39 -0300

    correção trava do tipo de jogo

--- app/Http/Controllers/Admin/Pages/Bets/GameController.php
+++ app/Http/Controllers/Admin/Pages/Bets/GameController.php
@@ -299,12 +299,6 @@ class GameController extends Controller
             $request['sort_date'] = Carbon::parse($request['sort_date'])->toDateTime();
 
             try {
-                $date = Carbon::now();
-                if ($date->hour >= 20 && $date->hour < 21) {
-                    return redirect()->route('admin.bets.games.create', ['type_game' => $request->type_game])->withErrors([
-                        'error' => 'Apostas Encerradas!'
-                    ]);
-                }
 
                 $balance = Balance::calculation($request->value);
 
