7d0f47a4392df1c10f95cc02d55baa89601ef3d9
Vinícius Castro <vinicastrolimavcl@gmail.com>
2024-03-07 18:18:51 -0300

    Feat=>SCraping de forma dinâmica da data

--- app/Http/Controllers/ScrapingController.php
+++ app/Http/Controllers/ScrapingController.php
@@ -8,36 +8,40 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class ScrapingController extends Controller
 {
-    public function scrape()
+    public function scrape($data)
     {
         $url = 'https://www.resultadosnahora.com.br/banca-ptm-rio/';
-
+    
         $client = new Client();
         $response = $client->get($url);
-
+    
         $html = $response->getBody()->getContents();
-
+    
         $crawler = new Crawler($html);
-
+    
         $targetTable = null;
 
-        $crawler->filter('table')->each(function ($table) use (&$targetTable) {
+        // dd($data);
+        $data = str_replace('-', '/', $data);
+
+    
+        $crawler->filter('table')->each(function ($table) use (&$targetTable, $data) {
             $tableContent = $table->html();
-            if (strpos($tableContent, 'Resultado Jogo do Bicho 1° ao 7° (PTM-Rio)') !== false) {
+            if (strpos($tableContent, 'Resultado Jogo do Bicho 1° ao 7° (PTM-Rio) 11:00 Hoje '. $data) !== false) {
                 $targetTable = $tableContent;
                 return false; // Para parar a iteração quando a tabela desejada for encontrada
             }
         });
-
+    
         if ($targetTable) {
             // Use DOMDocument para organizar os dados da tabela
             $doc = new \DOMDocument();
             $doc->loadHTML($targetTable);
-
+    
             $rows = $doc->getElementsByTagName('tr');
-
+    
             $data = [];
-
+    
             foreach ($rows as $row) {
                 $cols = $row->getElementsByTagName('td');
                 $rowData = [];
@@ -46,13 +50,14 @@ class ScrapingController extends Controller
                 }
                 $data[] = $rowData;
             }
-
+    
             // Remova a primeira linha que contém cabeçalhos
             array_shift($data);
-
+    
             return response()->json($data);
         } else {
             return response()->json(['error' => 'Tabela específica não encontrada'], 404);
         }
     }
+    
 }
