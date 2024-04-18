300afe670b883aec9fc7eb528c92e2225759cfa1
Raphael <Faelpinheiroantunes@gmail.com>
2024-04-18 16:54:35 -0300

    tela 1

--- resources/views/admin/pages/dashboards/selectresult/index.blade.php
+++ resources/views/admin/pages/dashboards/selectresult/index.blade.php
@@ -4,101 +4,97 @@
 
 @section('content')
 
-{{-- formulario onde buscaremos uma data especifica --}}
 
-<div class="container mt-5">
-    <div class="card-deck container d-flex justify-content-between card-header" style="padding: 30px;">
-        <div class="col-md-6 text-md-start ">
-            <h3 style="margin:0;">Lista de Ganhadores</h3>
-        </div>
-        <div class="col-md-6 d-flex align-items-center flex-md-row flex-column">
-            <h4 style="margin:0;" class="mr-2 mt-3 mt-md-0">Data:</h4>
-            <input class="form-control date" id="dataInput" type="date">
-        </div>
-    </div>
-</div>
 
 {{-- interface dos cards --}}
-<div class="card-deck container card-master" style="width: 100%; margin-bottom: 30px; margin-left: auto;
+<div class="container">
+    <img src="https://i.ibb.co/JQrky55/360-F-419131523-Qb-Mk-KL8h-THMx-B15-Vluf-Qb-ETEf59-Otzj-A.jpg"
+        style="width:100%;max-height:150px;">
+
+</div>
+<div class="card-deck container card-master" style="width: 100%; margin-bottom: 10px; margin-left: auto;
     margin-right: auto; margin-top:30px">
 
     <div class="card mb-6" style="background-color:#202223;">
-        <div class="card-header">Bilhetes Sorteados</div>
+        <div class="card-header">Bilhetes Totais</div>
         <div class="card-body">
-            <h5 class="card-title" style="font-size: 30px" id="campobilhetes"></h5> <i
-                class="nav-icon fas fa-chart-line" style="float: right; font-size: 50px; color:#98C715;"></i>
-            <p class="card-text"></p>
+            <h5 class="card-title" style="font-size: 30px" id="campobilhetes">124 bilhetes</h5> <i
+                class="nav-icon fa fa-ticket" style="float: right; font-size: 50px; color:#98C715;"></i>
         </div>
     </div>
 
     <div class="card mb-6" style="background-color:#202223;">
-        <div class="card-header">Premiações de Hoje</div>
+        <div class="card-header">Premiações Totais</div>
         <div class="card-body">
-
-            <h5 class="card-title" style="font-size: 30px" id="campopremiacoes"></h5> <i
+            <h5 class="card-title" style="font-size: 30px" id="campopremiacoes">R$ 22.300,00</h5> <i
                 class="nav-icon fas fa-dollar-sign" style="float: right; font-size: 50px;color:#98C715;"></i>
-            <p class="card-text"></p>
         </div>
     </div>
 
 </div>
+{{-- formulario onde buscaremos uma data especifica --}}
 
-<div class="container d-flex flex-md-row flex-column">
 
-    <div class="col-md-6 card-header">
-        <h3 style="margin:0;">Apostas Vencedoras</h3>
-    </div>
-    <div class="col-md-3 card-2" style="max-height:57px" id="copiarTexto">
-        <button class="btn btn-secondary mr-md-3 mr-2 d-flex justify-content-center align-items-center "
-            style="border-radius: 90px;">
-            <i style="color:#a3d712" class="fa fa-files-o wallet-nav"></i>
-        </button>
-        <p style="margin:0;" >Copiar Lista</p>
-    </div>
-    <div id="envianozap" class="col-md-3 card-2" style="max-height:57px">
-        <button class="btn btn-secondary mr-md-3 mr-2 d-flex justify-content-center align-items-center "
-            style="border-radius: 90px;">
-            <i style="color:#a3d712" class="fa fa-whatsapp wallet-nav"></i>
-        </button>
-        <p style="margin:0;">Enviar ao WhatsApp</p>
-    </div>
+<div class="container mt-1">
+    <div class="card-deck container d-flex justify-content-between card-header">
+        <div class="col-md-6 text-md-start ">
+            <h3 style="margin:0;">Concursos Sorteados</h3>
+        </div>
+        <div class="col-md-6 d-flex align-items-center justify-content-end flex-md-row flex-column">
+            <div class="d-flex justify-content-center align-items-center "
+            <div class="d-flex justify-content-center align-items-center "
+                style="background:#222425;border-radius:10px;">
+                <select class="form-control date">
+                    <option value="24">Ultimas 24 horas</option>
+                    <option value="48">Ultimos 2 dias</option>
+                    <option value="72">Ultimos 3 dias</option>
+                    <option value="168">Ultimos 7 dias</option>
+                </select>
+                <i class="nav-icon fa fa-clock-o ml-2" style="float: right; font-size: 20px;color:#98C715;"></i>
+            </div>
+        </div>
 
+    </div>
 </div>
 
-{{-- tabela de rede --}}
-<div class="container card-master mt-3">
-    
-    <table id="relatorio" class="table table-striped" style="width: 100%">
-        <div class="d-flex justify-content-end">
-            <button id="downloadPDF" class="btn btn-primary mb-2">PDF Ganhadores</button>
+<!-- Todos os jogos -->
+@if(\App\Models\TypeGame::count() > 0)
+<div class="container mt-3">
 
+    <div class="d-flex flex-wrap justify-content-center">
+        @php
+        $typeGames = \App\Models\TypeGame::get();
+        $count = 0;
+        @endphp
+
+        @foreach($typeGames as $typeGame)
+        <div class="d-flex p-2 box-imgs">
+            <a href="" class="hover-container">
+                <img class="img-todos-jogos" style="border-radius: 10px; width: 100%; height: 100%; object-fit: cover;"
+                    src="{{ $typeGame->banner_mobile ? asset(" storage/{$typeGame->banner_mobile}") :
+                asset('https://i.ibb.co/0yB31KB/60-Yp-Ckw9vf-EZXF9-Md4la52d-BK5j-YUPfqjx-E6c-Pro.jpg') }}"
+                alt="{{ $typeGame->name }} " >
+                <div class="hover-content">
+                    <p>{{ $typeGame->name }}</p>
+                    <button class="btn btn-primary">Selecionar</button>
+                </div>
+            </a>
         </div>
-        <thead>
-            <tr>
-                <th>{{ trans('admin.network-sales.table-id-header') }}</th>
-                <th>{{ trans('admin.network-sales.table-name-header') }}</th>
-                <th>Prêmio</th>
-                <th>Bilhetes</th>
-                <th>Modalidade</th>
-            </tr>
-        </thead>
-        <tbody>
 
-        </tbody>
-    </table>
+
+        @endforeach
+        @endif
+    </div>
 </div>
 
 <div class="p-3"></div>
 
 @endsection
 
-@push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.13/jspdf.plugin.autotable.min.js"></script>
 <script>
-
-    
     $(document).ready(function() {
         // Inicializa o DataTable
         var table = $('#relatorio').DataTable({
@@ -320,4 +316,63 @@ function listaall(dataSelecionada) {
         
     });
 </script>
-@endpush
+
+<style>
+    .hover-container {
+        position: relative;
+    }
+
+    .hover-content {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        display: none;
+        background-color: rgba(0, 0, 0, 0.7);
+        padding: 20px;
+        border-radius: 10px;
+        color: #fff;
+        box-sizing: border-box;
+    }
+
+    .hover-container:hover .hover-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .hover-content p {
+
+        font-weight: 700;
+
+    }
+
+    .hover-button {
+        padding: 10px;
+        background-color: #3498db;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+    }
+
+
+
+    @media screen and (max-width: 1400px) {
+        .hover-content {
+            padding: 10px !important;
+        }
+
+        .hover-content p {
+            font-size: 12px;
+
+        }
+
+        .hover-content button {
+            font-size: 10px;
+            padding: 5px;
+
+        }
+    }
+</style>
