c9493f2f9e3d42c44bac38a6a397ae8796a5de58
Dener Gomes <52760360+alodener@users.noreply.github.com>
2024-08-01 23:05:29 -0300

    Update ProcessBetEntries.php

--- app/Jobs/ProcessBetEntries.php
+++ app/Jobs/ProcessBetEntries.php
@@ -181,12 +181,13 @@ class ProcessBetEntries implements ShouldQueue
                     }
                 }
             }
+            $userTransact = User::find(auth()->id());
             $transact_balance = new TransactBalance;
             $transact_balance->user_id_sender = auth()->id();
             $transact_balance->user_id = auth()->id();
             $transact_balance->value = $game->value;
-            $transact_balance->old_value = auth()->user()->balance;
-            $transact_balance->value_a = auth()->user()->balance - $game->value;
+            $transact_balance->old_value = $userTransact->balance;
+            $transact_balance->value_a = $userTransact->balance - $game->value;
             $transact_balance->type = 'Compra - Jogo de id: ' . $game->id . ' do tipo: ' . $game->type_game_id;
             $transact_balance->save();
 
