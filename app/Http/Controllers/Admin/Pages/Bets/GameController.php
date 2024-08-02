d459558355d4bfbe8dd0b8f5b84bc4415b51d1c3
Dener Gomes <52760360+alodener@users.noreply.github.com>
2024-08-01 23:02:20 -0300

    Update GameController.php

--- app/Http/Controllers/Admin/Pages/Bets/GameController.php
+++ app/Http/Controllers/Admin/Pages/Bets/GameController.php
@@ -467,13 +467,13 @@ class GameController extends Controller
                         }
                     }
                 }
-
+                $userTransact = User::find(auth()->id());
                 $transact_balance = new TransactBalance;
                 $transact_balance->user_id_sender = auth()->id();
                 $transact_balance->user_id = auth()->id();
                 $transact_balance->value = $request->value;
-                $transact_balance->old_value = auth()->user()->balance;
-                $transact_balance->value_a = auth()->user()->balance - $request->value;
+                $transact_balance->old_value = $userTransact->balance;
+                $transact_balance->value_a = $userTransact->balance - $request->value;
                 $transact_balance->type = 'Compra - Jogo de id: ' . $game->id . ' do tipo: ' . $game->type_game_id;
                 $transact_balance->save();
 
