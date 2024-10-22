184ab608d1cb613761c55b6586b4aaa2a8cdfe79
Dener Gomes <52760360+alodener@users.noreply.github.com>
2024-10-22 19:30:51 -0300

    Update salesbichao.blade.php

--- resources/views/admin/layouts/pdf/salesbichao.blade.php
+++ resources/views/admin/layouts/pdf/salesbichao.blade.php
@@ -76,10 +76,7 @@
             $users = \App\Models\User::pluck('name', 'id');
             $total = 0;
         @endphp
-
-        @foreach($collection as $index => $userGames)
-            
-            <table style="width: 100%">
+<table style="width: 100%">
                 <tr class="bg-secondary">
                     <th class="text-size-1 text-left">ID</th>
                     <th class="text-size-1 text-left">CRIAÇÃO</th>
@@ -90,6 +87,9 @@
                     <th class="text-size-1 text-left">POSIÇÃO</th>
                     <th class="text-size-1 text-left">VALOR</th>
                 </tr>
+        @foreach($collection as $index => $userGames)
+            
+            
 
                 @php
                     $subtotal = 0;
